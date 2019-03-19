11a8939327e3399cd7d8681085921a6ae2bdcc14
Anjali A <bootcamper247@gmail.com>
2019-03-19 13:44:36 -0400

    initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# giftastic
-Animated Giphy images
+##### Georgia Tech Coding Boot Camp at Prototype Prime
+
+# GifTastic
+## HTML/CSS/JQuery/API
+
+- Here's how the app works:
+
+- When the user clicks on a button, the page grabs 10 static, non-animated gif images from the GIPHY API and places them on the page.
+
+- When the user clicks one of the still GIPHY images, the gifs animate. If the user clicks the gif again, it stops playing.
+
+- Each Image has its rating displayed above it.
+
+- The user can enter value to a user input box and that button also gets added to the page.
+
+
---
+++ assets/javascript/giftastic.js
@@ -0,0 +1,119 @@
+$(document).ready(function () {
+
+
+    const key = "dc6zaTOxFJmzC";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?rating=g&api_key=" + key + "&limit=10&q=";
+
+    var topics = ["Tennis", "Swimming", "Soccer", "Skiing", "Nascar", "Hockey"];
+
+
+    createButtons();
+
+
+    // Event handler for user clicking the topic button
+    $(document.body).on("click", ".btnTopic", function () {
+        // $(".btnTopic").on("click", function () {
+
+
+        $("#imagesDiv").empty();
+
+        var searchStr = $(this).attr("data-name");
+        var newURL = queryURL + searchStr;
+
+        // Run Query and process data
+        $.ajax({
+            url: newURL,
+            method: "GET"
+            // We store all of the retrieved data inside of an object called "response"
+        }).then(function (response) {
+
+            displayGifs(response)
+        });
+    });
+
+    //Event handler for submit button
+    $("#btnSubmit").on("click", function (event) {
+
+        //Preventing the button from trying to submit the form
+        event.preventDefault();
+
+        var sport = $("#sport").val().trim().toLowerCase();
+        if (sport != "") {
+            topics.push(sport);
+            createButtons();
+        }
+        $("#sport").val(" ");
+    });
+
+
+    function displayGifs(response) {
+        for (var i = 0; i < 10; i++) {
+
+            // Read the properties
+            var image_still = response.data[i].images.fixed_height_still.url;
+            var image_animate = response.data[i].images.fixed_height.url;
+            var rating = response.data[i].rating;
+
+            // Create the HTML Tags
+            var gifDiv = $("<div id = 'gifDiv' >");
+            var displayImage = $("<img>");
+            var displayRating = $("<h6>");
+
+            // Set their attributes
+            displayRating.attr("id", "idRating");
+            displayRating.text("Rating: " + rating.toUpperCase());
+            
+            displayImage.attr("src", image_still);
+            displayImage.attr("data-stillURL", image_still);
+            displayImage.attr("data-animatedURL", image_animate);
+            displayImage.attr("data-state", "still");
+            displayImage.attr("alt", "Sports Picture");
+            displayImage.addClass("gifs");
+
+            // Appending the image and rating to the gifDiv
+            displayRating.appendTo(gifDiv);
+            displayImage.appendTo(gifDiv);
+
+            // render gifDiv onto HTML page
+            $("#imagesDiv").append(gifDiv);
+        }
+    }
+
+    //create buttons
+    function createButtons() {
+
+        $("#buttonsDiv").empty();
+
+        for (var i = 0; i < topics.length; i++) {
+
+            var newBtn = $('<button class = "btn btn-info btnTopic" >');
+
+            //customize buttons
+            newBtn.text(topics[i].toUpperCase());
+            newBtn.attr("data-name", topics[i].toLowerCase());
+            newBtn.css("margin", "10px");
+            // newBtn.addClass("btn btn-primary");
+
+            //append buttons
+            newBtn.appendTo($("#buttonsDiv"));
+        }
+    }
+
+    $(document.body).on("click", ".gifs", toggle);
+
+    function toggle() {
+
+        var state = $(this).attr("data-state");
+        var stillURL = $(this).attr("data-stillURL");
+        var animatedURL = $(this).attr("data-animatedURL");
+
+        if (state == "still") {
+            $(this).attr("src", animatedURL);
+            $(this).attr("data-state", "animate");
+        } else if (state == "animate") {
+            $(this).attr("src", stillURL);
+            $(this).attr("data-state", "still");
+        }
+    };
+
+});     //end of document.ready
